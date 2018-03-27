306ec0e0905144487c15b2c2043308257da6b14c
bjciri\dev <907888614@qq.com>
2018-03-27 11:12:12 +0800

    ciri动态

--- src/components/news/CIRI-News/CIRI-News.vue
+++ src/components/news/CIRI-News/CIRI-News.vue
@@ -2,6 +2,12 @@
 
 <template>
   <div class="news">
+    <div class="scene">
+      <p class="coverage">
+        <span class="title">目前国内市场几乎空白。从国外市场来看，英国BBC</span>
+        <span class="summary">目前国内市场几乎空白。从国外市场来看，英国BBC</span>
+      </p>
+    </div>
     <tab-bar></tab-bar>
     <div class="main">
       <ul class="tab">
@@ -34,42 +40,42 @@
       TabBar,
       CrossLine
     },
-    data () {
-      return {
-      }
+    data() {
+      return {}
     },
     props: {},
     watch: {},
-    methods: {
-
-    },
+    methods: {},
     filters: {},
     computed: {},
-    created () {
+    created() {
     },
-    mounted () {},
-    destroyed () {}
+    mounted() {
+    },
+    destroyed() {
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style type="text/scss" lang="scss" scoped>
   @import '~@/assets/scss/const.scss';
   @import '~@/assets/scss/mixin.scss';
 
-  .gradient{
-    background: -webkit-linear-gradient(left, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Safari 5.1 - 6.0 */
-    background: -o-linear-gradient(right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Opera 11.1 - 12.0 */
-    background: -moz-linear-gradient(right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Firefox 3.6 - 15 */
-    background: linear-gradient(to right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* 标准的语法 */
+  .gradient {
+    background: -webkit-linear-gradient(left, rgba(56, 185, 253, 1), rgba(63, 132, 230, 0.65)); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(right, rgba(56, 185, 253, 1), rgba(63, 132, 230, 0.65)); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(right, rgba(56, 185, 253, 1), rgba(63, 132, 230, 0.65)); /* Firefox 3.6 - 15 */
+    background: linear-gradient(to right, rgba(56, 185, 253, 1), rgba(63, 132, 230, 0.65)); /* 标准的语法 */
   }
-  .tab{
+
+  .tab {
     @include onepx('bottom');
     width: 100%;
     height: 35px;
     text-align: center;
     display: flex;
     flex-direction: row;
-    li{
+    li {
       flex: 1;
       line-height: 35px;
       a {
@@ -84,5 +90,34 @@
     }
   }
 
+  .scene {
+    position: relative;
+    height: 186px;
+    width: 100%;
+    background: url('https://pic.36krcnd.com/201803/27015727/6krovf1y678bw4cm!heading') no-repeat;
+    .coverage {
+      height: 55px;
+      width: 100%;
+      background: #333 no-repeat;
+      opacity: 0.5;
+      position: absolute;
+      bottom:0;
+      .title {
+        color: #fff;
+        text-align: left;
+        display: block;
+        margin-top: 8px;
+        font-weight: bold;
+
+      }
+      .summary {
+        color: #fff;
+        margin-top: 6px;
+        display: block;
+        text-align: left;
+        margin-bottom: 10px;
+      }
+    }
+  }
 
 </style>
