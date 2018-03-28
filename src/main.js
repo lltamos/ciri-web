60034c6d3a78ff62aded4b597fb9ad13ef40c717
bjciri\dev <907888614@qq.com>
2018-03-28 09:48:41 +0800

    main.js 调整

--- src/main.js
+++ src/main.js
@@ -4,31 +4,33 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-Vue.config.productionTip = false
+// 将全局样式文件写在 main.js
+import '@/assets/scss/reset.scss'
+// Mint UI
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// 图片懒加载
+import VueLazyload from 'vue-lazyload'
 
 // vue-resource
 // 在服务器通讯方面，在升级到 vue 2.0 后，官方已经取消对 vue-resource 的更新而是转推 Axios
 import VueResource from 'vue-resource'
+Vue.config.productionTip = false
 Vue.use(VueResource)
-Vue.prototype.axios=axios
-// 将全局样式文件写在 main.js
-import '@/assets/scss/reset.scss'
-
-// Mint UI
-import Mint from 'mint-ui'
-import 'mint-ui/lib/style.css'
+Vue.prototype.axios = axios
 Vue.use(Mint)
-import tool from "./api/tool";
-
-// 图片懒加载
-import VueLazyload from 'vue-lazyload'
 Vue.use(VueLazyload, {
   // loading: '/static/loading-bars.svg'
   loading: require('@/assets/img/logo.png')
 })
-new Vue({
+let vm = new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
+})
+
+Vue.use({
+  vm
 })
