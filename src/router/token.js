9cebc2f1b18e916a228b74bdb4fbfd3412f258e4
fangfei_mei <18610289381@163.com>
2018-04-09 16:01:45 +0800

    前端路由拦截

--- src/router/token.js
+++ src/router/token.js
@@ -2,6 +2,23 @@ import axios from "axios"
 import tool from '@/api/tool'
 import router from './index'
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+    if (tool.gettoken()) {  // 通过vuex state获取当前的token是否存在
+      next();
+    }
+    else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      })
+    }
+  }
+  else {
+    next();
+  }
+})
+
 axios.defaults.headers.token = tool.gettoken()
 
 axios.interceptors.request.use((config) => {
@@ -27,6 +44,8 @@ axios.interceptors.response.use(function (response) {
   // Do something with response error
   return Promise.reject(error)
 })
+
+
 export default {
 
 }
