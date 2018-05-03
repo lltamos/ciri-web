242ecd93e43e49d8e97de2ed7ea2f9a1f2bae12f
xunan <xunan317@163.com>
2018-05-03 09:14:02 +0800

    消息页面

--- src/components/msg/msg.vue
+++ src/components/msg/msg.vue
@@ -1,8 +1,23 @@
 <!-- news -->
 
 <template>
-  <div class="">
-    <div>news</div>
+  <div class="msg">
+    <header class="gradient">源合资讯
+      <i class="icon_search"></i>
+    </header>
+    <div class="main">
+      <ul class="tab">
+        <li>
+          <router-link to="/msg/station">站内信</router-link>
+        </li>
+        <li>
+          <router-link to="/msg/system-notification">系统通知</router-link>
+        </li>
+      </ul>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
 
     <tab-bar></tab-bar>
   </div>
@@ -37,6 +52,87 @@
 <style lang="scss" scoped>
   @import '~@/assets/scss/const.scss';
   @import '~@/assets/scss/mixin.scss';
+  header {
+    height: 44px;
+    line-height: 44px;
+    color: #fff;
+    font-size: 20px;
+    text-align: center;
+    position: relative;
+
+    .icon_search {
+      display: block;
+      height: 22px;
+      width: 22px;
+      @include bg-image('../news/img/search');
+      background-size: 22px auto;
+      position: absolute;
+      right: 10px;
+      top: 11px;
+
+    }
+
+  }
+  .gradient{
+    background: -webkit-linear-gradient(left, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* Firefox 3.6 - 15 */
+    background: linear-gradient(to right, rgba(56,185,253,1) ,rgba(63,132,230,0.65)); /* 标准的语法 */
+  }
+
+  .tab{
+    width: 100%;
+    height: 33px;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    margin-top: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #dedede;
+    justify-content: center;
+    li:first-child{
+      line-height: 33px;
+      a {
+        display: block;
+        font-size: 15px;
+        color: #333;
+        width: 126px;
+        height: 33px;
+        border-top-left-radius: 3px;
+        border-bottom-left-radius:3px;
+        border-top: 1px solid #528deb;
+        border-left: 1px solid #528deb;
+        border-bottom: 1px solid #528deb;
+        &.router-link-active {
+          color: #fff;
+          background-color: #528de8;
+        }
+      }
+    }
+    li:last-child{
+      line-height: 33px;
+      a {
+        display: block;
+        font-size: 15px;
+        color: #333;
+        width: 126px;
+        height: 33px;
+        border-top-right-radius: 3px;
+        border-bottom-right-radius:3px;
+        border-top: 1px solid #528deb;
+        border-right: 1px solid #528deb;
+        border-bottom: 1px solid #528deb;
+        &.router-link-active {
+          color: #fff;
+          background-color: #528de8;
+        }
+      }
+    }
+
+
+  }
+
 
 
 </style>
+p
---
+++ src/components/msg/station/station.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="station">
+    <ul class="tab clearfix">
+      <li :class="{active:tabActive==1}" @click="showInbox">收件箱 <em>14</em></li>
+      <li :class="{active:tabActive==2}" @click="showOutbox">发件箱 <em></em></li>
+    </ul>
+    <div class="inbox outbox" v-show="seeInbox">
+      <div class="outbox-page">
+        <div class="point fl"></div>
+        <div class="message mar">
+          <div class="top-message" @click="showInDetail">
+            <span class="title">[<router-link to="">LiZhuQing</router-link>]申请查看项目: </span>[<router-link to="">巴西矿物质水工厂项目</router-link>]<span class="time">【 2018-03-16 09:41:43】</span>
+          </div>
+          <div class="bottom-message" v-show="isShow">
+            <p>
+              [LiZhuQing]对您的项目[巴西矿物质水工厂项目]感兴趣, 申请查看该项目的文件[<router-link to="">COSTA BEBER DRAFT TEASER CIRI 2017.pdf</router-link>], 以进一步了解该项目. 如有任何疑问，欢迎垂询 +86 010-8146-4345(工作时间9:00-18:30)
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+    <div class="outbox" v-show="!seeInbox">
+      <div class="outbox-page">
+        <div class="message">
+          <div class="top-message" @click="showOutDetail">
+            <span class="title">[<router-link to="">LiZhuQing</router-link>]申请查看项目: </span>[<router-link to="">巴西矿物质水工厂项目</router-link>]<span class="time">【 2018-03-16 09:41:43】</span>
+          </div>
+          <div class="bottom-message" v-show="isShow">
+            <p>
+              [LiZhuQing]对您的项目[巴西矿物质水工厂项目]感兴趣, 申请查看该项目的文件[<router-link to="">COSTA BEBER DRAFT TEASER CIRI 2017.pdf</router-link>], 以进一步了解该项目. 如有任何疑问，欢迎垂询 +86 010-8146-4345(工作时间9:00-18:30)
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="outbox-page">
+        <div class="message">
+          <div class="top-message">
+            <span class="title">[<router-link to="">LiZhuQing</router-link>]申请查看项目: </span>[<router-link to="">巴西矿物质水工厂项目</router-link>]<span class="time">【 2018-03-16 09:41:43】</span>
+          </div>
+          <div class="bottom-message">
+            <p>
+              [LiZhuQing]对您的项目[巴西矿物质水工厂项目]感兴趣, 申请查看该项目的文件[<router-link to="">COSTA BEBER DRAFT TEASER CIRI 2017.pdf</router-link>], 以进一步了解该项目. 如有任何疑问，欢迎垂询 +86 010-8146-4345(工作时间9:00-18:30)
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import tool from "@/api/tool";
+  export default {
+    components: {
+
+    },
+    data () {
+      return {
+        tabActive : 1,
+        seeInbox: true,
+        isShow:false,
+      }
+    },
+    props: {},
+    watch: {},
+    methods: {
+      showInbox(){
+        this.tabActive = 1;
+        this.seeInbox = true;
+      },
+      showOutbox(){
+        this.tabActive = 2;
+        this.seeInbox = false;
+      },
+      showOutDetail(){
+        this.isShow = !this.isShow
+      },
+      showInDetail(){
+        this.isShow = !this.isShow
+      },
+
+    },
+    filters: {},
+    computed: {},
+    created () {
+    },
+    mounted () {},
+    destroyed () {}
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/assets/scss/const.scss';
+  @import '~@/assets/scss/mixin.scss';
+  .station{
+    padding: 0 10px;
+    .tab{
+      margin: 15px 0 0px;
+      li{
+        float:left;
+        margin-right: 15px;
+        color:#528de8;
+        font-size: 10px;
+        width:75px;
+        height:20px;
+        line-height: 20px;
+        border-radius: 20px;
+        border: 1px solid #528de8;
+        text-align: center;
+        &.active{
+          background:#528de8 ;
+          color:#fff;
+        }
+
+      }
+    }
+    .inbox{
+      text-align: left;
+      .point{
+        width: 11px;
+        height: 11px;
+        background-color: red;
+        border-radius: 50%;
+        position: absolute;
+        margin-top: 5px;
+      }
+      .mar{
+        margin-left: 20px;
+      }
+    }
+    .outbox{
+      text-align: left;
+      .outbox-page{
+        border-bottom: 1px solid #dedede;
+        padding: 20px 0px;
+        font-size: 13px;
+        position: relative;
+        .point-read{
+          background-color: #ccc;
+        }
+        .message{
+          a{
+            color: #528de8;
+            text-decoration: underline;
+            margin: 0 3px;
+            &:hover,&:visited, &:link, &:active{
+              color: #528de8;
+            }
+          }
+          .top-message{
+            font-size: 14px;
+            cursor: pointer;
+            color: black;
+            .title{
+              line-height: 20px;
+            }
+            .time{
+              line-height: 20px;
+              color: #666;
+            }
+
+          }
+          .bottom-message{
+            margin-top: 5px;
+            color: #666;
+            font-size: 12px;
+            p{
+              line-height: 20px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+</style>
