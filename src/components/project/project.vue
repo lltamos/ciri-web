b27447da9de3e641f084b74f7a65c4bcb7c3d384
fangfei_mei <18610289381@163.com>
2018-05-15 16:48:06 +0800

    项目列表页bug修改

--- src/components/project/project.vue
+++ src/components/project/project.vue
@@ -56,7 +56,8 @@
                v-show=" index == num" :key="index">
             <form @submit.prevent="submit">
               <ul>
-                <li :id="all(index)" @click="allActive($event ,index)" :class="{active:activeSwitch}">全部
+                <!--:class="{active:activeSwitch}"-->
+                <li :id="all(index)" @click="allActive($event ,index)" class="active">全部
                   <input type="checkbox" value="全部"/>
                 </li>
                 <li v-for='(item,t) in itemCon' @click="liActive($event,item[0] ,index)" :id="t" :name="'li'+index"
@@ -251,6 +252,11 @@
 
       },
       liActive(e, v, index) {
+        let all=document.getElementById('all'+index);
+        if(all.classList.contains('active')){
+          all.classList.remove('active');
+        }
+        console.log(index);
         let element = e.currentTarget;
         if (element.classList.contains('active')) {
           element.classList.remove('active');
@@ -704,7 +710,6 @@
         }
       }
       .main {
-        min-height: 300px;
         padding: 0 10px;
         .pro-list {
           padding: 15px 0 5px 96px;
