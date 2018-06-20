df6a91bcc4b20fa01662dd65a0df7fd66c720174
zhouhy <bigdreamers@yeah.net>
2018-06-20 11:57:12 +0800

    genxin

--- src/api/tool.js
+++ src/api/tool.js
@@ -3,9 +3,9 @@ import moment from 'moment'
 
 export default {
   domind: function () {
-    return 'http://60.205.7.211:8816'
+    //return 'http://60.205.7.211:8816'
      //return 'http://127.0.0.1:8080'
-     //  return 'http://test.api.bjciri.com'
+     return 'http://test.api.bjciri.com'
     // return 'http://api.bjciri.com'
   },
   path: function () {
